b3ee6a267bb330515081c37140500f39063ce5d7
Akanksha <akankshakh00@gmail.com>
2023-12-10 23:50:07 +0530

    setting server, getting response

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-con
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+console.log('user hit the resource');    
+res.status(200).send('Homepage');
+})
+app.get ('/contact', (req,res)=>{
+res.status(200).send('Contact Page Bro');
+})
+app.all('*', (req, res)=>{
+res.status(404).send('<h1>Resource not found</h1>')    
+})
+app.listen(5000, () => { 
+    console.log('server is listening on port 5000');
+})
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.use
+// app.all
+// app.listen
